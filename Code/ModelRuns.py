50e356fd70ab8a74187f2f31278d95a250d25488
Andy Boomer <aboomer07@gmail.com>
2020-11-23 22:07:40 -0800

    commit

--- Code/ModelRuns.py
+++ Code/ModelRuns.py
@@ -3,72 +3,177 @@
 # Use this script to try out different model runs and compare them.
 ##########################################################################
 
+import warnings
+from ModelParams import *
+from ModelFunc import *
+from DataPrep import *
+from collections import OrderedDict
+
 # import data and functions
 # import libraries
-from collections import OrderedDict
-from Code.DataPrep import *
-from Code.ModelFunc import *
-from Code.ModelParams import *
-import warnings
 warnings.filterwarnings('ignore')
 
 # we use ordered dicts to store estimation results
-res_dict = OrderedDict()
+pool_dict = OrderedDict()
+fe_dict = OrderedDict()
+bb_dict = OrderedDict()
 
-# 1. Run: Pooled and FE model, bdbest25, no interactions
-res_dict['Pooled'] = run_model(master, pooled_bdbest25)
-res_dict['FE'] = run_model(master, fe_bdbest25)
+# 1. Run Pooled OLS Model with all interaction variants
+print("============================================================")
+print("Running Pooled Model")
+print("============================================================")
+pool_dict['Pooled'] = run_model(master, pool_armed)
 
-# 2. Run: Pooled and FE model, bdbest25,  interactions
-res_dict['PooledInteract'] = run_model(master, pooled_bdbest25_child)
-res_dict['FEInteract'] = run_model(master, fe_bdbest25_child)
+print("============================================================")
+print("Running Pooled Model Interacted with Child Mortality")
+print("============================================================")
+pool_dict['Pooled_ChildMortality'] = run_model(master, pool_armed_child)
 
-# export 1. Run and 2. Run
-compare_file = currDir + "/Report/OLS_armed.tex"
-# Andy used last set of model params for export?
-out_latex(res_dict, all_labs, fe_bdbest25_child, compare_file, "custom")
+print("============================================================")
+print("Running Pooled Model Interacted with Democracy")
+print("============================================================")
+pool_dict['Pooled_Democracy'] = run_model(master, pool_armed_democ)
+
+print("============================================================")
+print("Running Pooled Model Interacted with GDP")
+print("============================================================")
+pool_dict['Pooled_GDP'] = run_model(master, pool_armed_gdp)
+
+print("============================================================")
+print("Running Pooled Model Interacted with Good Index")
+print("============================================================")
+pool_dict['Pooled_GoodIndex'] = run_model(master, pool_armed_goodex)
+
+print("============================================================")
+print("Outputting Pooled OLS Models to Latex")
+print("============================================================")
+pool_file = currDir + "/Report/Pooled_armed.tex"
+out_latex(pool_dict, all_labs, pool_armed, pool_file, 'custom')
 
 
-# 3. Run: Pooled and FE model, bdbest1000, no interactions
-res_dict['Pooled'] = run_model(master, pooled_bdbest1000)
-res_dict['FE'] = run_model(master, fe_bdbest1000)
+# 1. Run FE Model with all interaction variants
+print("============================================================")
+print("Running FE Model")
+print("============================================================")
+fe_dict['FE'] = run_model(master, fe_armed)
 
-# 4. Run: Pooled and FE model, bdbest1000,  interactions
-res_dict['PooledInteract'] = run_model(master, pooled_bdbest1000_child)
-res_dict['FEInteract'] = run_model(master, fe_bdbest1000_child)
+print("============================================================")
+print("Running FE Model Interacted with Child Mortality")
+print("============================================================")
+fe_dict['FE_ChildMortality'] = run_model(master, fe_armed_child)
 
-# export 3. Run and 4. Run
-compare_file = currDir + "/Report/OLS_civil.tex"
-# Andy used last set of model params for export?
-out_latex(res_dict, all_labs, fe_bdbest1000_child, compare_file, "custom")
+print("============================================================")
+print("Running FE Model Interacted with Democracy")
+print("============================================================")
+fe_dict['FE_Democracy'] = run_model(master, fe_armed_democ)
+
+print("============================================================")
+print("Running FE Model Interacted with GDP")
+print("============================================================")
+fe_dict['FE_GDP'] = run_model(master, fe_armed_gdp)
+
+print("============================================================")
+print("Running FE Model Interacted with Good Index")
+print("============================================================")
+fe_dict['FE_GoodIndex'] = run_model(master, fe_armed_goodex)
+
+print("============================================================")
+print("Outputting FE Models to Latex")
+print("============================================================")
+fe_file = currDir + "/Report/FE_armed.tex"
+out_latex(fe_dict, all_labs, fe_armed, fe_file, 'custom')
 
 
-# 5. Run: Blundell bond bdbest25
-gmm_dict = OrderedDict()
-gmm_dict['GMM'] = blundell_bond(master, bb_bdbest25)
+# 1. Run Blundell-Bond Model with all interaction variants
+print("============================================================")
+print("Running Blundell-Bond Model")
+print("============================================================")
+bb_dict['BB'] = run_model(master, bb_armed)
 
-gmm_file = currDir + "/Report/GMM_armed.tex"
-out_latex(gmm_dict, all_labs, bb_bdbest25, gmm_file, "custom")
+print("============================================================")
+print("Running Blundell-Bond Model Interacted with Child Mortality")
+print("============================================================")
+bb_dict['BB_ChildMortality'] = run_model(master, bb_armed_child)
 
-gmm_fd_file = currDir + "/Report/GMM_FD_armed.tex"
-gmm_dict['GMM_FD'] = blundell_bond(master, bb_bdbest25_FD)
-out_latex(gmm_dict, all_labs, bb_bdbest25_FD, gmm_fd_file, "custom")
+print("============================================================")
+print("Running Blundell-Bond Model Interacted with Democracy")
+print("============================================================")
+bb_dict['BB_Democracy'] = run_model(master, bb_armed_democ)
 
-# 6. Run: Blundell bond bdbest25 child
-gmm_dict = OrderedDict()
-gmm_dict['GMM'] = blundell_bond(master, bb_bdbest25_child)
+print("============================================================")
+print("Running Blundell-Bond Model Interacted with GDP")
+print("============================================================")
+bb_dict['BB_GDP'] = run_model(master, bb_armed_gdp)
 
-# 7. Run: Blundell bond bdbest1000
-gmm_dict = OrderedDict()
-gmm_dict['GMM'] = blundell_bond(master, bb_bdbest1000)
+print("============================================================")
+print("Running Blundell-Bond Model Interacted with Good Index")
+print("============================================================")
+bb_dict['BB_GoodIndex'] = run_model(master, bb_armed_goodex)
 
-gmm_file = currDir + "/Report/GMM_civil.tex"
-out_latex(gmm_dict, all_labs, bb_bdbest1000, gmm_file, "custom")
+print("============================================================")
+print("Outputting Blundell-Bond Models to Latex")
+print("============================================================")
+bb_file = currDir + "/Report/BB_armed.tex"
+out_latex(bb_dict, all_labs, bb_armed, bb_file, 'custom')
+
 
 # Export exemplary ROC for baseline fe bdbest 25
-fe_bdbest25['onset'] = False
+print("============================================================")
+print("Computing Basic FE Model ROC Curve")
+print("============================================================")
 file = currDir + "/Report/ROC_FE.png"
-compute_roc(master, fe_bdbest25, file)
+compute_roc(master, fe_armed, file)
 
+print("============================================================")
+print("Computing FE Model Interacted with Child Mortality ROC Curve")
+print("============================================================")
+file = currDir + "/Report/ROC_FE_child.png"
+compute_roc(master, fe_armed_child, file)
+
+print("============================================================")
+print("Computing FE Model Interacted with Democracy ROC Curve")
+print("============================================================")
+file = currDir + "/Report/ROC_FE_democ.png"
+compute_roc(master, fe_armed_democ, file)
+
+print("============================================================")
+print("Computing FE Model Interacted with GDP ROC Curve")
+print("============================================================")
+file = currDir + "/Report/ROC_FE_gdp.png"
+compute_roc(master, fe_armed_gdp, file)
+
+print("============================================================")
+print("Computing FE Model Interacted with Good Index ROC Curve")
+print("============================================================")
+file = currDir + "/Report/ROC_FE_goodex.png"
+compute_roc(master, fe_armed_goodex, file)
+
+print("============================================================")
+print("Computing Basic Blundell-Bond Model ROC Curve")
+print("============================================================")
 file = currDir + '/Report/ROC_BB.png'
-compute_roc(master, bb_bdbest25, file)
+compute_roc(master, bb_armed, file)
+
+print("============================================================")
+print("Computing Blundell-Bond Model Interacted with Child Mortality ROC Curve")
+print("============================================================")
+file = currDir + '/Report/ROC_BB_child.png'
+compute_roc(master, bb_armed_child, file)
+
+print("============================================================")
+print("Computing Blundell-Bond Model Interacted with Democracy ROC Curve")
+print("============================================================")
+file = currDir + '/Report/ROC_BB_democ.png'
+compute_roc(master, bb_armed_democ, file)
+
+print("============================================================")
+print("Computing Blundell-Bond Model Interacted with GDP ROC Curve")
+print("============================================================")
+file = currDir + '/Report/ROC_BB_gdp.png'
+compute_roc(master, bb_armed_gdp, file)
+
+print("============================================================")
+print("Computing Blundell-Bond Model Interacted with Good Index ROC Curve")
+print("============================================================")
+file = currDir + '/Report/ROC_BB_goodex.png'
+compute_roc(master, bb_armed_goodex, file)
