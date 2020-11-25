b671e2029a5ca2448cba2b8ecadd37cb369e950d
Andy Boomer <aboomer07@gmail.com>
2020-11-24 23:02:39 -0800

    Making a few changes, adding graphs, adjusting how parameters are set

--- Code/ModelParams.py
+++ Code/ModelParams.py
@@ -9,7 +9,7 @@ print(interactions)
 # got to data prep if you want to change the set of interaction terms that can be used
 
 # baseline pooled bdbest25
-pool_armed = {
+pool_armed_onset = {
     "fit_year": 2013,  # Year for fitting the model
     "dep_var": "bdbest25",  # Civil War (1000) or Armed Conflict (25)
     "onset": True,  # Onset of Incidence of Conflict
@@ -21,16 +21,10 @@ pool_armed = {
     'iterations': 2,  # How many iterations for system gmm
     'topic_cols': ['theta' + str(i) for i in range(1, 15)],  # theta cols
     'weight_type': 'unadjusted',  # Type of gmm weighting matrix
-    'FD': False
 }
 
-pool_armed_child = pool_armed.copy()
-pool_armed_goodex = pool_armed.copy()
-pool_armed_democ = pool_armed.copy()
-pool_armed_gdp = pool_armed.copy()
-
 # baseline fixed effects bdbest25
-fe_armed = {
+fe_armed_onset = {
     "fit_year": 2013,  # Year for fitting the model
     "dep_var": "bdbest25",  # Civil War (1000) or Armed Conflict (25)
     "onset": True,  # Onset of Incidence of Conflict
@@ -42,26 +36,7 @@ fe_armed = {
     'iterations': 2,  # How many iterations for system gmm
     'topic_cols': ['theta' + str(i) for i in range(1, 15)],  # theta cols
     'weight_type': 'unadjusted',  # Type of gmm weighting matrix
-    'FD': False
 }
-
-fe_armed_child = fe_armed.copy()
-fe_armed_goodex = fe_armed.copy()
-fe_armed_democ = fe_armed.copy()
-fe_armed_gdp = fe_armed.copy()
-
-# Start changing interactions terms to compare
-pool_armed_child['interactions'] = ['childmortality']
-pool_armed_goodex['interactions'] = ['avegoodex']
-pool_armed_democ['interactions'] = ['democracy']
-pool_armed_gdp['interactions'] = ['rgdpl']
-
-# Start changing interactions terms to compare
-fe_armed_child['interactions'] = ['childmortality']
-fe_armed_goodex['interactions'] = ['avegoodex']
-fe_armed_democ['interactions'] = ['democracy']
-fe_armed_gdp['interactions'] = ['rgdpl']
-
 
 # baseline blundell bond bdbest25
 bb_armed = {
@@ -71,22 +46,45 @@ bb_armed = {
     "all_indiv": True,  # Include all countries or not
     "FE": False,  # Pooled Model or Fixed Effects
     'interactions': None,  # Set of interaction vars (can be None)
-    'max_lags': 3,  # Set max lags for instrumental variables
+    'max_lags': 2,  # Set max lags for instrumental variables
     'lagged_regs': True,  # Define whether to use lagged labels
-    'iterations': 100,  # How many iterations for system gmm
+    'iterations': 5,  # How many iterations for system gmm
     'topic_cols': ['theta' + str(i) for i in range(1, 15)],  # theta cols
-    'weight_type': 'unadjusted',  # Type of gmm weighting matrix
-    'FD': False
+    'weight_type': 'unadjusted'  # Type of gmm weighting matrix
 }
 
-bb_armed_child = bb_armed.copy()
-bb_armed_goodex = bb_armed.copy()
-bb_armed_democ = bb_armed.copy()
-bb_armed_gdp = bb_armed.copy()
+dep_map = {'armed': 'bdbest25', 'civil': 'bdbest1000', 'deaths': 'bdbest'}
+inter_map = {'child': 'childmortality', 'democ': 'democracy', 'gdp': 'rgdpl',
+             'good': 'avegoodex'}
+params = {}
+for mod in ['pool', 'fe', 'bb']:
+    params[mod] = {}
+    for dep in ['armed', 'civil', 'deaths']:
+        params[mod][dep] = {}
+        if mod == 'bb':
+            params[mod][dep]['Init'] = bb_armed.copy()
+            params[mod][dep]['Init']['dep_var'] = dep_map[dep]
 
-# blundell bond with interaction bdbest 1000
-bb_armed_child = bb_armed.copy()
-bb_armed_child['interactions'] = ['childmortality']
-bb_armed_goodex['interactions'] = ['avegoodex']
-bb_armed_democ['interactions'] = ['democracy']
-bb_armed_gdp['interactions'] = ['rgdpl']
+        for var in ['onset', 'incidence']:
+            if mod == 'pool':
+                params[mod][dep][var] = {}
+                params[mod][dep][var]['Init'] = pool_armed_onset.copy()
+                params[mod][dep][var]['Init']['dep_var'] = dep_map[dep]
+                params[mod][dep][var]['Init']['onset'] = [
+                    False, True][var == 'onset']
+            elif mod == 'fe':
+                params[mod][dep][var] = {}
+                params[mod][dep][var]['Init'] = fe_armed_onset.copy()
+                params[mod][dep][var]['Init']['dep_var'] = dep_map[dep]
+                params[mod][dep][var]['Init']['onset'] = [
+                    False, True][var == 'onset']
+            for inter in ['child', 'democ', 'gdp', 'good']:
+                if mod == 'bb':
+                    params[mod][dep][inter] = params[mod][dep]['Init'].copy(
+                    )
+                    params[mod][dep][inter]['interactions'] = [inter_map[inter]]
+                else:
+                    params[mod][dep][var][inter] = params[mod][dep][var]['Init'].copy(
+                    )
+                    params[mod][dep][var][inter]['interactions'] = [
+                        inter_map[inter]]
